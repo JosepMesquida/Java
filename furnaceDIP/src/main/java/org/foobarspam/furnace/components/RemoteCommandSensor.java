8293520dcd7809981f0cb44eeddc8dbe95c02a09
josep <josep@joseppc>
2017-03-23 23:43:04 +0100

    Modificación GasHeater

--- furnaceDIP/src/main/java/org/foobarspam/furnace/components/RemoteCommandSensor.java
+++ furnaceDIP/src/main/java/org/foobarspam/furnace/components/RemoteCommandSensor.java
@@ -3,10 +3,10 @@ package org.foobarspam.furnace.components;
 import org.foobarspam.furnace.interfaces.Thermometer;
 
 public class RemoteCommandSensor implements Thermometer {
-	
-    @Override
-    public double read(AmbientTemperature temperature) {
-        return temperature.getTemperature();
-}
+
+	@Override
+	public double read(AmbientTemperature temperature) {
+		return temperature.getTemperature();
+	}
 
 }
